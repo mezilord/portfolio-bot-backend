737ac3bfa6e8c224caa4395759b5f4404cdf16d0
Muhammad Zafar <mzafar611@gmail.com>
2023-10-08 09:02:48 +0500

    commit

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json())
 
 // Configure open api
 const configuration = new Configuration({
+    organization: "org-iYrpkxSTgDINm7N4mmEcqaC1",
     apiKey: process.env.API_KEY // VISIT .env AND MAKE CHANGES
 })
 const openai = new OpenAIApi(configuration)
